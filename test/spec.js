71ab6b497a9edd6de98e6ffd9d227f93fba237ac
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-07-23 14:58:01 +0200

    definition of library behaviour with tests

--- test/spec.js
+++ test/spec.js
@@ -1,11 +1,29 @@
 'use strict';
 
-var assert = require('assert');
-var lib = require('../lib/messenger.js');
+var chai = require('chai').should()
+  , Messenger = require('../lib/messenger.js').Messenger;
 
-describe('Basic library test', function () {
-  it('should answer all questions with YO!', function () {
-    var answer = lib.Messenger('Should I tickle this unicorn?');
-    assert.equal(answer, 'YO!');
+describe('Messenger library', function () {
+  describe('instance', function () {
+    it('should create an object with pub/sub standard methods');
+  });
+
+  describe('channel subscribing', function () {
+    it('should fail for a missing channel');
+    it('should fail for a missing callback');
+    it('should return a token when used');
+  });
+
+  describe('channel publishing', function () {
+    it('shouldn\'t fail if there aren\'t any subscribers');
+    it('shouldn\'t fail if the channel doesn\'t exist');
+    it('should publish and call all related callbacks');
+    it('should publish and call related callbacks with arguments');
+  });
+
+  describe('channel unsubscribing', function () {
+    it('should fail for a missing token argument');
+    it('should fail if token is not valid');
+    it('should unsubscribe from this channel with a valid token');
   });
 });
